9c878daa8315cef4debdc76da0a8e1b6d22fb4c7
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-19 12:10:37 +0300

    add functions for business login

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var add = function(number1, number2){
+    return number1 + number2;
+};
+
+var substract = function(number1, number2){
+    return number1 - number2;
+};
+var multiply = function(number1,number2){
+    return number1 * number2;
+};
+
